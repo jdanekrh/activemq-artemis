f6375a8f9d877508ec7d1fb5bcd7e63bb7e3d121
Jiri Danek <jdanek@redhat.com>
2018-07-12 22:58:59 +0200

    bazel: BUILD for artemis-selector
    There are some misplaced dependency declarations directly in BUILD file
    and there is first use of list comprehension to generate test suite.

---
+++ artemis-selector/javacc/JavaCC.java
@@ -0,0 +1,6 @@
+public class JavaCC {
+    public static void main(String[] args) throws Exception {
+        // https://github.com/mojohaus/javacc-maven-plugin/blob/2dce60227484e6944618dc16470d85ea5db7eb22/src/main/java/org/codehaus/mojo/javacc/JavaCC.java#L483
+        org.javacc.parser.Main.mainProgram( args );
+    }
+}
